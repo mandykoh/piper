acb73181ee576a10472fb04c71330c6e0bfb3843
Amanda Koh <amanda@naucera.net>
2017-05-24 15:54:46 +1000

    Implement flattening with Cartesian product.

--- flatteningsource.go
+++ flatteningsource.go
@@ -3,32 +3,70 @@ package piper
 import "reflect"
 
 type flatteningSource struct {
-	sourceStack []Source
+	source             Source
+	currentValues      []reflect.Value
+	indexables         []reflect.Value
+	indexablePositions []int
+	indexes            []int
 }
 
-func (s *flatteningSource) Next() (values []reflect.Value, ok bool) {
-
+func (s *flatteningSource) Next() ([]reflect.Value, bool) {
 	for {
-		currentSourceIndex := len(s.sourceStack) - 1
-		if currentSourceIndex < 0 {
+		values, ok := s.nextValuesFromIndexables()
+		if ok {
+			return values, true
+		}
+
+		newValues, ok := s.source.Next()
+		if !ok {
 			return nil, false
 		}
 
-		currentSource := s.sourceStack[currentSourceIndex]
-		values, ok = currentSource.Next()
-		if ok {
-
-			if len(values) == 1 {
-				subSource, ok := values[0].Interface().(Source)
-				if ok {
-					s.sourceStack = append(s.sourceStack, subSource)
-					continue
-				}
-			}
-
-			return
-		}
-
-		s.sourceStack = s.sourceStack[:currentSourceIndex]
+		s.resetIndexables(newValues)
 	}
+}
+
+func (s *flatteningSource) nextValuesFromIndexables() (values []reflect.Value, ok bool) {
+
+	if s.indexables == nil {
+		return nil, false
+	}
+
+	for i := 0; i < len(s.indexables); i++ {
+		value := s.indexables[i].Index(s.indexes[i])
+		s.currentValues[s.indexablePositions[i]] = value
+	}
+
+	for i := len(s.indexables) - 1; ; {
+		s.indexes[i]++
+		if s.indexes[i] >= s.indexables[i].Len() {
+			s.indexes[i] = 0
+			i--
+			if i < 0 {
+				s.indexables = nil
+				break
+			}
+		} else {
+			break
+		}
+	}
+
+	return s.currentValues, true
+}
+
+func (s *flatteningSource) resetIndexables(values []reflect.Value) {
+	s.currentValues = values
+
+	s.indexables = []reflect.Value{}
+	s.indexablePositions = []int{}
+
+	for i := 0; i < len(values); i++ {
+		switch values[i].Kind() {
+		case reflect.Array, reflect.Slice:
+			s.indexables = append(s.indexables, values[i])
+			s.indexablePositions = append(s.indexablePositions, i)
+		}
+	}
+
+	s.indexes = make([]int, len(s.indexables))
 }
--- flatteningsource_test.go
+++ flatteningsource_test.go
@@ -5,17 +5,14 @@ import (
 	"testing"
 )
 
-func TestFlatteningSourceReturnsElementsFromUnderlyingSourcesOfSources(t *testing.T) {
+func TestFlatteningSourceFormsCartesianProductOfMultipleReturnValues(t *testing.T) {
 
 	s := flatteningSource{
-		sourceStack: []Source{
-			&indexableSource{indexable: reflect.ValueOf([]Source{
-				&indexableSource{indexable: reflect.ValueOf([]string{"a", "b"})},
-				&indexableSource{indexable: reflect.ValueOf([]string{"c", "d"})},
-				&indexableSource{indexable: reflect.ValueOf([]Source{
-					&indexableSource{indexable: reflect.ValueOf([]string{"e", "f"})},
-				})},
-			})},
+		source: projectedSource{
+			source: &indexableSource{indexable: reflect.ValueOf([]string{"dummy"})},
+			projection: reflect.ValueOf(func(v string) ([]string, string, []string) {
+				return []string{"1", "2"}, "x", []string{"a", "b"}
+			}),
 		},
 	}
 
@@ -24,8 +21,84 @@ func TestFlatteningSourceReturnsElementsFromUnderlyingSourcesOfSources(t *testin
 	if !ok {
 		t.Fatal("Expected an element but none come next")
 	}
+	if result[0].String() != "1" {
+		t.Errorf("Expected element '1' but got %v", result[0])
+	}
+	if result[1].String() != "x" {
+		t.Errorf("Expected element 'x' but got %v", result[1])
+	}
+	if result[2].String() != "a" {
+		t.Errorf("Expected element 'a' but got %v", result[2])
+	}
+
+	result, ok = s.Next()
+
+	if !ok {
+		t.Fatal("Expected a second element but none come next")
+	}
+	if result[0].String() != "1" {
+		t.Errorf("Expected element '1' but got %v", result[0])
+	}
+	if result[1].String() != "x" {
+		t.Errorf("Expected element 'x' but got %v", result[1])
+	}
+	if result[2].String() != "b" {
+		t.Errorf("Expected element 'b' but got %v", result[2])
+	}
+
+	result, ok = s.Next()
+
+	if !ok {
+		t.Fatal("Expected a third element but none come next")
+	}
+	if result[0].String() != "2" {
+		t.Errorf("Expected element '2' but got %v", result[0])
+	}
+	if result[1].String() != "x" {
+		t.Errorf("Expected element 'x' but got %v", result[1])
+	}
+	if result[2].String() != "a" {
+		t.Errorf("Expected element 'a' but got %v", result[2])
+	}
+
+	result, ok = s.Next()
+
+	if !ok {
+		t.Fatal("Expected a fourth element but none come next")
+	}
+	if result[0].String() != "2" {
+		t.Errorf("Expected element '2' but got %v", result[0])
+	}
+	if result[1].String() != "x" {
+		t.Errorf("Expected element 'x' but got %v", result[1])
+	}
+	if result[2].String() != "b" {
+		t.Errorf("Expected element 'b' but got %v", result[2])
+	}
+
+	result, ok = s.Next()
+
+	if ok {
+		t.Fatalf("Expected no more elements but got %v", result)
+	}
+}
+
+func TestFlatteningSourceUnwrapsSlicesFromUnderlyingSource(t *testing.T) {
+
+	s := flatteningSource{
+		source: &indexableSource{indexable: reflect.ValueOf([][]string{
+			[]string{"a", "b"},
+			[]string{"c", "d"},
+		})},
+	}
+
+	result, ok := s.Next()
+
+	if !ok {
+		t.Fatal("Expected an element but none come next")
+	}
 	if result[0].String() != "a" {
-		t.Fatalf("Expected element 'a' but got %v", result)
+		t.Errorf("Expected element 'a' but got %v", result[0])
 	}
 
 	result, ok = s.Next()
@@ -34,7 +107,7 @@ func TestFlatteningSourceReturnsElementsFromUnderlyingSourcesOfSources(t *testin
 		t.Fatal("Expected a second element but none come next")
 	}
 	if result[0].String() != "b" {
-		t.Fatalf("Expected element 'b' but got %v", result)
+		t.Errorf("Expected element 'b' but got %v", result[0])
 	}
 
 	result, ok = s.Next()
@@ -43,7 +116,7 @@ func TestFlatteningSourceReturnsElementsFromUnderlyingSourcesOfSources(t *testin
 		t.Fatal("Expected a third element but none come next")
 	}
 	if result[0].String() != "c" {
-		t.Fatalf("Expected element 'c' but got %v", result)
+		t.Errorf("Expected element 'c' but got %v", result[0])
 	}
 
 	result, ok = s.Next()
@@ -52,25 +125,7 @@ func TestFlatteningSourceReturnsElementsFromUnderlyingSourcesOfSources(t *testin
 		t.Fatal("Expected a fourth element but none come next")
 	}
 	if result[0].String() != "d" {
-		t.Fatalf("Expected element 'd' but got %v", result)
-	}
-
-	result, ok = s.Next()
-
-	if !ok {
-		t.Fatal("Expected a fifth element but none come next")
-	}
-	if result[0].String() != "e" {
-		t.Fatalf("Expected element 'e' but got %v", result)
-	}
-
-	result, ok = s.Next()
-
-	if !ok {
-		t.Fatal("Expected a sixth element but none come next")
-	}
-	if result[0].String() != "f" {
-		t.Fatalf("Expected element 'f' but got %v", result)
+		t.Errorf("Expected element 'd' but got %v", result[0])
 	}
 
 	result, ok = s.Next()
